a8a6de11edeb8bdc2e0976589835fa5c4f5b4218
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-08-13 17:44:08 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:".... a story",
+        options:["tell", "play", "explore", "solve"],
+        answer: 0
+    },
+    {
+        q:".... a magnifying glass",
+        options:["play", "use", "overcome", "smoke"],
+        answer: 1
+    },
+    {
+        q:".... an area",
+        options:["overcome", "put", "explore", "tell"],
+        answer: 2
+    },
+    {
+        q:".... a role",
+        options:["explore", "solve", "tell", "play"],
+        answer: 3
+    },
+    {
+        q:".... a character",
+        options:["create", "explore", "solve", "wear"],
+        answer: 0
+    },
+    {
+        q:".... criminals behind bars",
+        options:["wear", "put", "explore", "overcome"],
+        answer: 1
+    },
+    {
+        q:".... a crime",
+        options:["put", "smoke", "solve", "tell"],
+        answer: 2
+    },
+    {
+        q:".... a pipe",
+        options:["wear", "overcome", "explore", "smoke"],
+        answer: 3
+    },
+    {
+        q:".... an obstacle",
+        options:["overcome", "explore", "smoke", "create"],
+        answer: 0
+    },
+    {
+        q:".... a hat",
+        options:["create", "wear", "solve", "smoke"],
+        answer: 1
+    }
+
+
+
+]
